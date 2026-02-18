a637761ac3a08a14620635820bafee1d3e0a0778
gustasbepirstis <gustasb@wix.com>
2026-02-18 16:46:03 +0200

    conli

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function iWillDefinitelyHaveConflicts(a, b) {
   const iWillCreateConflicts = () => {
-    const _a = a + 1;
-    const _b = b + 2;
-    return _a + _b;
+    return a + b;
   }
   
   return iWillCreateConflicts();
 }
 
+console.log(iWillDefinitelyHaveConflicts(10, 12));
 console.log(iWillDefinitelyHaveConflicts(10, 12));
